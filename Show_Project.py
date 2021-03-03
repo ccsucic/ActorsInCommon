74ca2f252ed61f8f480746262cdd5416a4be7ba6
Conner Sucic <52499085+ccsucic@users.noreply.github.com>
2021-03-02 18:08:48 -0600

    Update Show_Project.py
    Removing unnecessary variables and comments

--- Show_Project.py
+++ Show_Project.py
@@ -1,20 +1,13 @@
-# -*- coding: utf-8 -*-
-"""
-Created on Wed Jan 22 21:37:35 2020
-
-@author: ccsuc
-"""
-
 import timeit
 import pandas as pd
 import numpy as np
 from functools import reduce
 from itertools import combinations
 
-df_com = pd.read_excel("C:/Users/ccsuc/Desktop/Personal_Projects/Show_Project_Folder/Community_Cast.xlsx")
-df_par = pd.read_excel("C:/Users/ccsuc/Desktop/Personal_Projects/Show_Project_Folder/Parks_And_Rec_Cast.xlsx")
-df_off = pd.read_excel("C:/Users/ccsuc/Desktop/Personal_Projects/Show_Project_Folder/Office_Cast.xlsx")
-df_roc = pd.read_excel("C:/Users/ccsuc/Desktop/Personal_Projects/Show_Project_Folder/30_Rock_Cast.xlsx")
+df_com = pd.read_excel("Community_Cast.xlsx")
+df_par = pd.read_excel("Parks_And_Rec_Cast.xlsx")
+df_off = pd.read_excel("Office_Cast.xlsx")
+df_roc = pd.read_excel("30_Rock_Cast.xlsx")
 
 data = [df_com["Name"], df_par["Name"], df_off["Name"], df_roc["Name"]]
 headers = ["Community Names", "ParksAndRec Names", "Office Names", "30 Rock Names"]
@@ -112,53 +105,7 @@ print("The time to compute using NumPy is: ",
 '''
 '''
 com_off_common = list(set(com_list) & set(off_list))
-com_roc_common = list(set(com_list) & set(roc_list))
-par_off_common = list(set(par_list) & set(off_list))
-par_roc_common = list(set(par_list) & set(roc_list))
-off_roc_common = list(set(off_list) & set(roc_list))
 com_par_off_common = list((set(set(com_list) & set(par_list))) & set(off_list))
-com_off_roc_common = list((set(set(com_list) & set(off_list))) & set(roc_list))
-com_par_roc_common = list((set(set(com_list) & set(par_list))) & set(roc_list))
-par_off_roc_common = list((set(set(par_list) & set(off_list))) & set(roc_list))
-com_par_off_roc_common = list((set((set(set(com_list) & set(par_list))) & set(off_list))) & set(roc_list))
-'''
-'''
-print("For Community: ", len(df_com.index))
-print("For Parks and Rec: ", len(df_par.index))
-print("For The Office: ", len(df_off.index))
-print("For 30 Rock: ", len(df_roc.index))
-
-com_list = df_com["Name"].to_list()
-par_list = df_par["Name"].to_list()
-off_list = df_off["Name"].to_list()
-roc_list = df_roc["Name"].to_list()
-
-com_par_common = list(set(com_list) & set(par_list))
-com_off_common = list(set(com_list) & set(off_list))
-com_roc_common = list(set(com_list) & set(roc_list))
-par_off_common = list(set(par_list) & set(off_list))
-par_roc_common = list(set(par_list) & set(roc_list))
-off_roc_common = list(set(off_list) & set(roc_list))
-com_par_off_common = list((set(set(com_list) & set(par_list))) & set(off_list))
-com_off_roc_common = list((set(set(com_list) & set(off_list))) & set(roc_list))
-com_par_roc_common = list((set(set(com_list) & set(par_list))) & set(roc_list))
-par_off_roc_common = list((set(set(par_list) & set(off_list))) & set(roc_list))
 com_par_off_roc_common = list((set((set(set(com_list) & set(par_list))) & set(off_list))) & set(roc_list))
 
-print("Community and Parks and Rec have", len(com_par_common),"actors in common")
-print("Community and The Office have", len(com_off_common), "actors in common")
-print("Community and 30 Rock have", len(com_roc_common), "actors in common")
-print("Parks and Rec and The Office have", len(par_off_common), "actors in common")
-print("Parks and Rec and 30 Rock have", len(par_roc_common), "actors in common")
-print("The Office and 30 Rock have", len(off_roc_common), "actors in common")
-print("Community, Parks and Rec, and The Office have", len(com_par_off_common), "actor in common")
-print("Community, The Office, and 30 Rock have",len(com_off_roc_common),"actors in common")
-print("Community, Parks and Rec, and 30 Rock have",len(com_par_roc_common),"actor in common")
-print("Parks and Rec, The Office, and 30 Rock have",len(par_off_roc_common),"actors in common")
-print(com_par_off_common, "was in Community, Parks and Rec, and The Office")
-print(com_off_roc_common, "were in Community, The Office, and 30 Rock") 
-print(com_par_roc_common, "was in Community, Parks and Recreation, and 30 Rock")
-print(par_off_roc_common, "were in Parks and Rec, The Office, and 30 Rock")
-print("And finally, the original question:")
-print(len(com_par_off_roc_common), "people were in Community, Parks and Rec, The Office, and 30 Rock ")
 '''
