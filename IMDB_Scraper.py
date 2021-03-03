5b7436e5e4b66c31bf69320bb8624dd705888cf6
Conner Sucic <52499085+ccsucic@users.noreply.github.com>
2021-03-02 19:31:45 -0600

    Found cast_list table

--- IMDB_Scraper.py
+++ IMDB_Scraper.py
@@ -1,4 +1,8 @@
 # This file will contain the Scrapy scraper used to scrape IMDB
-# Code to come shortly!
 
 import scrapy
+
+# Gives the table and row of cast_list table in Scrapy shell (not finished)
+table = response.xpath('//*[@class="cast_list"]//tr')
+row = table[1]
+row.xpath('td//text()').extract()
